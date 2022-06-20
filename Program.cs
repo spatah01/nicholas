e353900610f59cb5ec0f70db4ecaf4f8ab2075fa
nicholas1 <mutisyanicholas11@gmail.com>
2022-06-20 19:52:29 +0300

    initial Commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+int x=100;
+int y=0;
+int result;
+try{
+    result=x/y;
+    Console.WriteLine("the result is:{0}" ,result);
+}
+catch{
+    Console.WriteLine("error");
+}
